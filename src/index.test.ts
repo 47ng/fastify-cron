0650bfd661f390c133106c2f29fe6eb0a8075c4c
gtolarc <gtolarc@gmail.com>
2020-07-08 14:01:56 +0900

    feat: support fastify3

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,115 +1,115 @@
-import 'jest-extended'
-import Fastify from 'fastify'
-import fastifyCron from './index'
+import 'jest-extended';
+import Fastify from 'fastify';
+import fastifyCron from './index';
 
 describe('Creating jobs at plugin registration time', () => {
   test('no config', async () => {
-    const server = Fastify({ logger: false })
-    server.register(fastifyCron)
-    await server.ready()
-    expect(server.cron.jobs).toBeArrayOfSize(0)
-  })
+    const server = Fastify({ logger: false });
+    server.register(fastifyCron);
+    await server.ready();
+    expect(server.cron.jobs).toBeArrayOfSize(0);
+  });
 
   test('jobs are not started after plugins are loaded', async () => {
-    const server = Fastify({ logger: false })
-    const spy = jest.fn()
-    server.register(fastifyCron, {
-      jobs: [
-        {
-          cronTime: '* * * * *',
-          onTick: spy
-        }
-      ]
-    })
-    await server.ready() // Wait for plugins to load
-    expect(server.cron.jobs[0].running).toBeFalsy()
-    expect(spy).not.toHaveBeenCalled()
-  })
-
-  test('start job when startWhenReady = true', async () => {
-    const server = Fastify({ logger: false })
-    const spy = jest.fn()
+    const server = Fastify({ logger: false });
+    const spy = jest.fn();
     server.register(fastifyCron, {
       jobs: [
         {
           cronTime: '* * * * *',
           onTick: spy,
-          startWhenReady: true
-        }
-      ]
-    })
-    await server.ready()
-    expect(server.cron.jobs[0].running).toBeTrue()
-    server.cron.stopAllJobs()
-  })
+        },
+      ],
+    });
+    await server.ready(); // Wait for plugins to load
+    expect(server.cron.jobs[0].running).toBeFalsy();
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  test('start job when startWhenReady = true', async () => {
+    const server = Fastify({ logger: false });
+    const spy = jest.fn();
+    server.register(fastifyCron, {
+      jobs: [
+        {
+          cronTime: '* * * * *',
+          onTick: spy,
+          startWhenReady: true,
+        },
+      ],
+    });
+    await server.ready();
+    expect(server.cron.jobs[0].running).toBeTrue();
+    server.cron.stopAllJobs();
+  });
 
   test('retrieve job by name', async () => {
-    const server = Fastify({ logger: false })
-    const spy = jest.fn()
+    const server = Fastify({ logger: false });
+    const spy = jest.fn();
     server.register(fastifyCron, {
       jobs: [
         {
           name: 'foo',
           cronTime: '* * * * *',
-          onTick: spy
-        }
-      ]
-    })
-    await server.ready()
-    const job = server.cron.getJobByName('foo')
-    expect(job).toBeDefined()
-    expect(job!.running).toBeFalsy()
-  })
-})
+          onTick: spy,
+        },
+      ],
+    });
+    await server.ready();
+    const job = server.cron.getJobByName('foo');
+    expect(job).toBeDefined();
+    expect(job!.running).toBeFalsy();
+  });
+});
 
 describe('Creating jobs manually after registration', () => {
   test('jobs created after `ready` event should start automatically', async () => {
-    const spy = jest.fn()
-    const server = Fastify({ logger: false })
-    server.register(fastifyCron)
-    await server.ready()
-    server.cron.createJob({
-      cronTime: '* * * * *',
-      onTick: spy
-    })
-
-    // We still need to re-await here as job startup is asynchronous,
-    // but all plugins have already been loaded at this point.
-    await server.ready()
-    expect(server.cron.jobs).toBeArrayOfSize(1)
-    expect(server.cron.jobs[0].running).toBeFalsy()
-    server.cron.stopAllJobs()
-  })
-
-  test('start job when startWhenReady = true', async () => {
-    const server = Fastify({ logger: false })
-    const spy = jest.fn()
-    server.register(fastifyCron)
-    await server.ready() // Needed to have the decoration
+    const spy = jest.fn();
+    const server = Fastify({ logger: false });
+    server.register(fastifyCron);
+    await server.ready();
     server.cron.createJob({
       cronTime: '* * * * *',
       onTick: spy,
-      startWhenReady: true
-    })
-    await server.ready() // Will resolve immediately
-    expect(server.cron.jobs[0].running).toBeTrue()
-    server.cron.stopAllJobs()
-  })
+    });
+
+    // We still need to re-await here as job startup is asynchronous,
+    // but all plugins have already been loaded at this point.
+    await server.ready();
+    expect(server.cron.jobs).toBeArrayOfSize(1);
+    expect(server.cron.jobs[0].running).toBeFalsy();
+    server.cron.stopAllJobs();
+  });
+
+  test('start job when startWhenReady = true', async () => {
+    const server = Fastify({ logger: false });
+    const spy = jest.fn();
+    server.register(fastifyCron);
+    await server.ready(); // Needed to have the decoration
+    server.cron.createJob({
+      cronTime: '* * * * *',
+      onTick: spy,
+      startWhenReady: true,
+    });
+    await server.ready(); // Will resolve immediately
+    expect(server.cron.jobs[0].running).toBeTrue();
+    server.cron.stopAllJobs();
+  });
 
   test('retrieve job by name', async () => {
-    jest.useFakeTimers()
-    const server = Fastify({ logger: false })
-    const spy = jest.fn()
-    server.register(fastifyCron)
-    await server.ready()
+    jest.useFakeTimers();
+    const server = Fastify({ logger: false });
+    const spy = jest.fn();
+    server.register(fastifyCron);
+    await server.ready();
     server.cron.createJob({
       name: 'foo',
       cronTime: '* * * * *',
-      onTick: spy
-    })
-    await server.ready()
-    const job = server.cron.getJobByName('foo')
-    expect(job).toBeDefined()
-    expect(job!.running).toBeFalsy()
-  })
-})
+      onTick: spy,
+    });
+    await server.ready();
+    const job = server.cron.getJobByName('foo');
+    expect(job).toBeDefined();
+    expect(job!.running).toBeFalsy();
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,37 @@
-import fp from 'fastify-plugin'
-import { FastifyInstance, FastifyError } from 'fastify'
-import { CronJob, CronJobParameters } from 'cron'
+import fp from 'fastify-plugin';
+import { FastifyPlugin } from 'fastify';
+import { CronJob, CronJobParameters } from 'cron';
 
 export type Job = CronJob & {
-  readonly name?: string
-}
+  readonly name?: string;
+};
 
 declare module 'fastify' {
   interface FastifyInstance {
-    cron: CronDecorator
+    cron: CronDecorator;
   }
 }
 
 export type Params = Omit<CronJobParameters, 'onTick' | 'onComplete'> & {
-  onTick<S>(server: S): void
-  onComplete?<S>(server: S): void
-  startWhenReady?: boolean
-  name?: string
-}
+  onTick<S>(server: S): void;
+  onComplete?<S>(server: S): void;
+  startWhenReady?: boolean;
+  name?: string;
+};
 
 export interface CronDecorator {
-  readonly jobs: Job[]
-  createJob(params: Params): Job
-  getJobByName(name: string): Job | undefined
-  startAllJobs(): void
-  stopAllJobs(): void
+  readonly jobs: Job[];
+  createJob(params: Params): Job;
+  getJobByName(name: string): Job | undefined;
+  startAllJobs(): void;
+  stopAllJobs(): void;
 }
 
 export interface Config {
-  jobs?: Params[]
+  jobs?: Params[];
 }
 
-function plugin<S extends FastifyInstance>(
-  server: S,
-  config: Config,
-  next: (err?: FastifyError) => void
-) {
+const plugin: FastifyPlugin<Config> = (server, opts, next) => {
   try {
     const decorator: CronDecorator = {
       jobs: [],
@@ -43,43 +39,40 @@ function plugin<S extends FastifyInstance>(
         const innerParams: CronJobParameters = {
           ...params,
           onTick: () => params.onTick(server),
-          onComplete: () =>
-            typeof params.onComplete === 'function'
-              ? () => params.onComplete!(server)
-              : undefined
-        }
+          onComplete: () => (typeof params.onComplete === 'function' ? () => params.onComplete!(server) : undefined),
+        };
         const job: Job = Object.assign(new CronJob(innerParams), {
-          name: params.name
-        })
+          name: params.name,
+        });
         if (params.startWhenReady === true) {
-          server.ready(() => job.start())
+          server.ready(() => job.start());
         }
-        decorator.jobs.push(job)
-        return job
+        decorator.jobs.push(job);
+        return job;
       },
       getJobByName: function getJobByName(name) {
-        return decorator.jobs.find(j => j.name === name)
+        return decorator.jobs.find((j) => j.name === name);
       },
       startAllJobs: function startAllJobs() {
-        decorator.jobs.forEach(job => job.start())
+        decorator.jobs.forEach((job) => job.start());
       },
       stopAllJobs: function stopAllJobs() {
-        decorator.jobs.forEach(job => job.stop())
-      }
-    }
-    for (const params of config.jobs || []) {
-      decorator.createJob(params)
+        decorator.jobs.forEach((job) => job.stop());
+      },
+    };
+    for (const params of opts.jobs || []) {
+      decorator.createJob(params);
     }
 
-    server.decorate('cron', decorator)
-    server.addHook('onClose', () => decorator.stopAllJobs())
-    next()
+    server.decorate('cron', decorator);
+    server.addHook('onClose', () => decorator.stopAllJobs());
+    next();
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 export default fp(plugin, {
   name: 'fastify-cron',
-  fastify: '2.x'
-})
+  fastify: '3.x',
+});
